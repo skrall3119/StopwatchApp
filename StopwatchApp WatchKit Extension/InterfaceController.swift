b2f2cc90be5dd685cb9b1929fc9a78222a7c45ff
skrall3119 <alexjanci@yahoo.com>
2022-02-25 18:51:31 -0600

    init

--- StopwatchApp WatchKit Extension/InterfaceController.swift
+++ StopwatchApp WatchKit Extension/InterfaceController.swift
@@ -1,6 +1,6 @@
 //
 //  InterfaceController.swift
-//  StopwatchApp WatchKit Extension
+//  stopwatch WatchKit Extension
 //
 //  Created by Alex Janci on 2/25/22.
 //
@@ -11,10 +11,31 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    var isStarted = false
+    
+    @IBOutlet weak var startStopButton: WKInterfaceButton!
+    @IBOutlet weak var timeLabel: WKInterfaceTimer!
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
+    @IBAction func StartStopTimer() {
+        isStarted = !isStarted
+        if isStarted {
+            startStopButton.setTitle("Stop")
+            resetTimer(timer: timeLabel, 0)
+        } else {
+            startStopButton.setTitle("Start")
+            timeLabel.stop()
+        }
+    }
+    func resetTimer(timer: WKInterfaceTimer, _ interval: TimeInterval){
+        timer.stop()
+        let time = Date(timeIntervalSinceNow: interval)
+        timer.setDate(time)
+        timer.start()
+    }
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
     }

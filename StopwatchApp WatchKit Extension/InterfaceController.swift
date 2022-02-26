bd46b41bcc650a1dd5616f080437a06dcc35131a
skrall3119 <alexjanci@yahoo.com>
2022-02-25 18:43:03 -0600

    Initial Commit

---
+++ StopwatchApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  StopwatchApp WatchKit Extension
+//
+//  Created by Alex Janci on 2/25/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
